5b37ddcd24c41a2dfc1f384b05b7bb4c62fdd001
flaviafafa <jiangxiaofafa@gmail.com>
2024-02-05 15:42:36 -0500

    took feedback survey down and changed profile

--- pages/index.tsx
+++ pages/index.tsx
@@ -233,74 +233,11 @@ const Home: NextPage = (props: any) => {
                         But before this...❗
                     </h2>
                     <p className="sm:text-lg sm:mx-[7%] lg:mx-[10%] text-center">
-                        ❗Make sure you have filled out the feedback survey for PM23 and checked out the new
-                        statistics &#40;see below&#41;❗
+                        ❗Make sure you have checked out the new {' '}
+                        <Link href="/statistics">
+                            <span className="underline font-bold cursor-pointer">Statistics</span>
+                        </Link>❗
                     </p>
-                </div>
-            </section>
-
-            <section className="grid lg:grid-cols-2">
-                <div className="bg-white">
-                    <div className="mt-12 lg:my-24 lg:mb-8">
-                        <div className="text-center lg:text-left mx-[3%] md:mx-[10%] lg:ml-[8%] lg:mr-[6%]">
-                            <h1 className="text-xl text-gray-600 font-extrabold md:text-3xl">
-                                Want an <span className="text-rose-400">extra match </span>for PM2024?
-                            </h1>
-                            <p className="mt-6 lg:mt-10 text-base md:text-lg text-gray-500">
-                                <strong className="font-bold text-rose-400">NEW: </strong>Give us feedback on your
-                                PM2023 matches and receive an additional match this year❣️
-                            </p>
-                            <p className="mt-2 md:text-lg text-gray-500">
-                                Sign in and head to your{' '}
-                                <Link href="/profile">
-                                    <span className="underline font-bold cursor-pointer">Profile</span>
-                                </Link>{' '}
-                                to complete the <strong className="font-bold text-rose-400">feedback survey</strong>.
-                            </p>
-                            <div className="flex">
-                                <div className="mt-4 flex flex-wrap content-center mx-auto lg:mx-0">
-                                    <GoogleAuth login={!props.user} />
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="lg:mt-4 mt-4 mb-10">
-                        <Image priority={true} src={demo} alt="Loading..."></Image>
-                    </div>
-                </div>
-
-                <div className="bg-pink-100">
-                    <div className="my-12 lg:my-24">
-                        <div className="text-center lg:text-left mx-[3%] md:mx-[10%] lg:ml-[8%] lg:mr-[6%]">
-                            <h1 className="text-xl text-gray-600 font-extrabold md:text-3xl">
-                                Curious about the results of <span className="text-rose-400">PM23? </span>
-                            </h1>
-                            <ul className="mt-6 lg:mt-10 md:text-lg text-gray-500">
-                                <li>🐻 3900+ Cornellians matched this year</li>
-                                <li>💌 22,000+ Perfect Matches</li>
-                                <li>
-                                    📊 Visit the{' '}
-                                    <Link href="/statistics">
-                                        <span className="underline font-bold cursor-pointer">Statistics</span>
-                                    </Link>{' '}
-                                    page for more about the results!
-                                </li>
-                            </ul>
-                            <div className="mt-6 lg:mt-12 md:text-lg">
-                                <p className="text-rose-400 font-bold">For example, we asked:</p>
-                                <p className="my-2 lg:my-4 text-gray-500">
-                                    Choose the <strong>best alternative</strong>. Your match is a 10, but they ____.
-                                </p>
-                                <p className="text-rose-400 font-bold">You chose:</p>
-
-                                <div className="content-center my-3">
-                                    <div className="max-w-xl mx-auto">
-                                        <BestAlternative />
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
                 </div>
             </section>
             <Footer />
--- pages/profile.tsx
+++ pages/profile.tsx
@@ -7,6 +7,7 @@ import { NextPage } from 'next';
 import { getSession } from 'next-auth/react';
 import { fetcher } from '@/utils/fetch';
 import useSWR from 'swr';
+import CountDown from '@/components/countdown';
 
 const Profile: NextPage = (props: any) => {
     const { data, error, mutate } = useSWR('/api/profile', fetcher);
@@ -18,45 +19,7 @@ const Profile: NextPage = (props: any) => {
                 <title>Profile</title>
             </Head>
             <Header />
-            <div>
-                <section className="text-gray-500">
-                    <div className="pt-12 sm:pt-28 mx-[2%] sm:mx-[15%] lg:mx-[22%] mx-auto">
-                        <h1 className="text-2xl sm:text-3xl font-extrabold text-rose-400 text-center">
-                            Welcome Back, {data.profile.firstName}!
-                        </h1>
-                        <div className="content-center flex">
-                            <p className="text-lg mt-8 mx-auto sm:text-xl text-center">
-                                <strong className="text-rose-400">NEW:</strong> Fill out the feedback survey below to
-                                receive an extra match in 2024❣️
-                            </p>
-                        </div>
-                        <p className="mt-6 text-center">
-                            We truly appreciate your feedback and comments! They will be used to improve next year&#39;s
-                            survey and matching algorithms. Please complete the two parts of the feedback survey:{' '}
-                            <strong className="text-rose-400">General Feedback and Match-Specific Feedback</strong>. It
-                            will take 5~10 minutes in total.
-                        </p>
-                        <p className="mt-6 text-center">
-                            {' '}
-                            <strong className="text-rose-400">Step 1 </strong>The General Feedback section is right
-                            below.
-                        </p>
-                        <p className="mt-6 text-center">
-                            <strong className="text-rose-400">Step 2 </strong>For Match-Specific Feedback, scroll down
-                            to each of your matches and click &#34;Leave Feedback.&#34;
-                        </p>
-                    </div>
-                </section>
-            </div>
-            <div>
-                <section>
-                    <div>
-                        <div className="bg-white rounded-lg h-auto">
-                            <ProfileTabs user={data} refresh={refresh} />
-                        </div>
-                    </div>
-                </section>
-            </div>
+            <CountDown />
             <Footer />
         </div>
     );
