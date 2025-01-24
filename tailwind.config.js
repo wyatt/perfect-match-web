b704203b4ae9088545b8614ec7dce69b6e82fea6
Nicholas Varela <nicholasjv@icloud.com>
2025-01-24 13:12:01 -0500

    Finished adding PM Colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,31 +3,28 @@ module.exports = {
     content: ['./app/**/*.{js,ts,jsx,tsx}', './pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
     theme: {
         extend: {
-            //custom colors
+            // custom colors
             colors: {
                 'pmpink': {
-
                     500: '#fce5f3'
-
                 },
                 'pmpink2': {
-
                     500: '#f7a4af'
-
                 },
                 'pmred': {
-
                     500: '#f30020'
-
-
                 },
                 'pmblue': {
                     500: '#24438d'
-
                 },
-
+                'pmorange': {
+                    500: '#FF7E55'
+                },
+                'pmblue2': {
+                    500: '#C5E1EF'
+                },
             },
         },
-        plugins: [],
-    }
+    },
+    plugins: [],
 };
