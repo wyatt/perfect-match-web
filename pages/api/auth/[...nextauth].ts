1747d1b78ea22c3e40a74a500128d2b370374180
Huajie Zhong <hz642@cornell.edu>
2025-02-03 16:44:44 -0500

    revert change in sign in

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -26,7 +26,7 @@ export default NextAuth({
             clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
         }),
     ],
-    pages: { signIn: '/auth/signin', error: '/auth/error' },
+    pages: { error: '/auth/error' },
     secret: process.env.NEXTAUTH_SECRET,
     callbacks: {
         async signIn({ profile }) {
