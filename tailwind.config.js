892cc2b3e3c345a5cdabb32109be7ed2645b43a9
Huajie Zhong <hz642@cornell.edu>
2025-02-11 20:20:53 -0500

    Add custom 3D transform utilities and flip animation to Tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+const plugin = require('tailwindcss/plugin');
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
     content: ['./app/**/*.{js,ts,jsx,tsx}', './pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
@@ -29,15 +31,41 @@ module.exports = {
                 'press-start': ['"Press Start 2P"', 'sans-serif'],
             },
             keyframes: {
-                flash : {
+                flash: {
                     '0%, 100%': { opacity: 1 },
                     '50%': { opacity: 0 },
+                },
+                flip: {
+                    '0%': { transform: 'rotateY(0)' },
+                    '100%': { transform: 'rotateY(0.5turn)' },
                 },
             },
             animation: {
                 flash: 'flash 1s steps(1, end) infinite',
-            }
+                flip: 'flip 1s ease-in-out infinite',
+            },
         },
     },
-    plugins: [],
+    plugins: [
+        plugin(function ({ addUtilities }) {
+            const newUtilities = {
+                '.perspective-400': {
+                    perspective: '400px',
+                },
+                '.transform-3d': {
+                    'transform-style': 'preserve-3d',
+                },
+                '.rotate-y-half': {
+                    transform: 'rotateY(0turn)',
+                },
+                '.rotate-y-half': {
+                    transform: 'rotateY(0.5turn)',
+                },
+                '.backface-hidden': {
+                    'backface-visibility': 'hidden',
+                },
+            };
+            addUtilities(newUtilities, ['responsive']);
+        }),
+    ],
 };
