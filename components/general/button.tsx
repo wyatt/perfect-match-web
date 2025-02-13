d34eaf9265a8678a783b4330118ea0442c5abc06
Huajie Zhong <hz642@cornell.edu>
2025-02-12 23:37:19 -0500

    refactor button component to use shadowWidth prop and improve default values

--- components/general/button.tsx
+++ components/general/button.tsx
@@ -9,14 +9,19 @@ interface ButtonProps {
     py?: number;
     mt?: number;
     border?: number;
-    shadow?: number;
+    shadowWidth?: number;
     bold?: boolean;
     className?: string;
 }
 
 const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
     (
-        { children, onClick, bgColor, textColor, px, py, mt, border, shadow, bold, className },
+        { children = '', onClick,
+            bgColor = 'bg-white', textColor = 'text-pmred-500',
+            px = 6, py = 2, mt = 6,
+            border = 4, shadowWidth = 6,
+            bold = true,
+            className = '' },
         ref
     ) => {
         return (
@@ -24,30 +29,32 @@ const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
                 ref={ref}
                 onClick={onClick}
                 className={`
-                    mt-${mt || 6}
-                    px-${px || 6}
-                    py-${py || 2}
+                    mt-${mt}
+                    px-${px}
+                    py-${py}
                     rounded-full
-                    ${bgColor || 'bg-white'}
-                    ${textColor || 'text-pmred-500'}
-                    border-${border || 4}
+                    ${bgColor}
+                    ${textColor}
+                    border-${border}
                     border-pmblue-500 
                     ${bold ? 'font-bold' : 'font-medium'}
-                    shadow-[${shadow || '6'}px_${shadow || '6'}px_0px_0px_rgba(36,67,141,1)]
+                    shadow-[${shadowWidth}px_${shadowWidth}px_0px_0px_rgba(36,67,141,1)]
                     transition-all
                     hover:translate-x-[4px]
                     hover:translate-y-[4px]
-                    hover:shadow-[2px_2px_0px_0px_rgba(36,67,141,1)]
+                    hover:shadow-[${shadowWidth - 4}px_${shadowWidth - 4}px_0px_0px_rgba(36,67,141,1)]
                     active:translate-x-[6px]
                     active:translate-y-[6px] 
                     active:shadow-none 
-                    ${className || ''}
+                    ${className}
                 `}
             >
-                {children || ''}
+                {children}
             </button>
         );
     }
 );
+
+Button.displayName = 'Button';
 
 export default Button;
